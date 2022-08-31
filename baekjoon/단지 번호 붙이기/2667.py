c07866010525db947dd4eca5c26bcf9602e1e3da
glass2300 <51581244+glass2300@users.noreply.github.com>
2022-08-31 18:19:08 +0900

    📝 [2022.08.31] 2667.py
    단지 번호 붙이기
    
    comment: 중복을 없애기 위한 여러 방법을 생각해보자

--- baekjoon/단지 번호 붙이기/2667.py
+++ baekjoon/단지 번호 붙이기/2667.py
@@ -1,31 +1,38 @@
-from operator import eq
-import sys
 from collections import deque
+import sys
 input = sys.stdin.readline
 
-N = int(input())
-graph = [list(map(int, input().rstrip())) for _ in range(N)]
+size = int(input())
 
+graph = [list(map(int, input().rstrip())) for _ in range(size)]
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x,y):
+def bfs():
     queue = deque()
-    queue.append((x,y))
-    graph[x][y] = 0
-    count = 0
+    totalGroup = []
+    groupSize = int(0)
+    for i in range(size):
+        for j in range(size):
+            if(graph[i][j] != 0):
+                queue.append((i,j))
+                graph[i][j] = 0
+                groupSize = 1
+            while queue:
+                a, b = queue.popleft()
+                
+                for k in range(4):
+                    nx = a + dx[k]
+                    ny = b + dy[k]
+                    if (nx >= 0 and ny >= 0 and nx <= (size-1) and ny <= (size-1) and graph[nx][ny] == 1):
+                        graph[nx][ny] = 0
+                        groupSize+=1
+                        queue.append((nx, ny))
+            if groupSize != 0: 
+                totalGroup.append(groupSize)
+                groupSize = 0
+    totalGroup.sort()
+    print(len(totalGroup))
+    print(*totalGroup, sep='\n')
 
-    while queue:
-        x, y = queue.popleft()
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-        if (nx >= 0 and ny >= 0 and nx < N and ny < N and graph[nx][ny] == 1):
-            graph[nx][ny] = 0
-            queue.append((x,y))
-            count += 1
-    return count
-
-print(bfs(0,0))
+bfs()
