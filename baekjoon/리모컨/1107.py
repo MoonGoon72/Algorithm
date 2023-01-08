d42912c019df596c44aa4fe91e7bc5faea34be9a
glass2300 <glass23000@naver.com>
2023-01-08 15:17:42 +0900

    📝 solve: 1107 리모컨 / 추후 다시 풀어볼 것

--- baekjoon/리모컨/1107.py
+++ baekjoon/리모컨/1107.py
@@ -1,12 +1,23 @@
-channel = input()
+# 다시 풀어볼 것
+
+channel = int(input())
 buttonNum = int(input())
 channelLen = len(str(channel))
-brokenButtons = list(map(int, input().split(" ")))
-totalPress = 0
+if buttonNum != 0:
+    brokenButtons = list(map(int, input().split(" ")))
+else:
+    print(channelLen)
+    exit
+result = abs(100 - channel)
 
-for i in range(channelLen, 0, -1):
-    tmpNum = channelLen // (10 ** i)
-    if tmpNum not in brokenButtons:
-        totalPress += 1
-    else:
-        
+
+for nums in range(1000001):
+    nums = str(nums)
+    for i in range(len(nums)):
+        # 누를 수 없는 수면 다음 수를 찾는다
+        if int(nums[i]) in brokenButtons:
+            break
+        # 모든 숫자를 누를 수 있으면 그 수에서 +나 - 버튼으로 이동하여 원하는 채널로 이동하는 총 입력수를 측정하고 더 작은수를 result에 넣어 갱신한다.
+        elif i == len(nums) -1:
+            result = min(result, abs(channel - int(nums)) + len(nums))
+print(result)
