722e74b82088ce84b716459e24c67190c17c2d68
MoonGoon <moongoon.cnu@gmail.com>
2023-01-21 23:03:31 +0900

    ✅  Solve: 1389.py

--- baekjoon/케빈 베이컨의 6단계 법칙/1389.py
+++ baekjoon/케빈 베이컨의 6단계 법칙/1389.py
@@ -20,7 +20,6 @@ for i in range(1, user+1):
     distance = [0 for _ in range(user + 1)]
     queue = deque()
     queue.append(i)
-
     while queue:
         now = queue.popleft()
         if not visited[now]:
@@ -30,12 +29,12 @@ for i in range(1, user+1):
                     distance[node] = distance[now] + 1
                 else:
                     # 현재 노드 + 1이 다음 노드의 최단거리보다 짧으면 갱신해준다
-                    if distance[now] + 1 < distance[node]: distance[node] = distance[now] + 1
-            for i in relations[now]:
-                queue.append(i)
+                    if distance[now] + 1 < distance[node]:
+                        distance[node] = distance[now] + 1
+            for num in relations[now]:
+                queue.append(num)
             visited[now] = True
-        # queue.append(relations[now]) # 배열이 들어가니 수정해아함
     distance[i] = 0
     totalLength[i] = sum(distance)
-print(totalLength)
-print(totalLength.index(min(totalLength)) + 1)
+    
+print(totalLength.index(min(totalLength)))
