a566e31df210426c2bf7bf44db2bb638e3541c58
MoonGoon <moongoon.cnu@gmail.com>
2023-01-25 21:47:46 +0900

    ✅ Solve: 1463.py

--- baekjoon/1로 만들기/1463.py
+++ baekjoon/1로 만들기/1463.py
@@ -1,24 +1,15 @@
+INF = float("inf")
 number = int(input())
 
-result = 0
+arr = [INF for _ in range(number + 1)]
 
-arr = [0 for _ in range(1000000)]
+arr[1] = 0
 
+for i in range(2, number + 1, 1):
+    tmp = INF
+    if i % 3 == 0: tmp = min(tmp, arr[i // 3])
+    if i % 2 == 0: tmp = min(tmp, arr[i // 2])
+    tmp = min(tmp, arr[i - 1]) + 1
+    arr[i] = tmp
 
-for i in range(1, 4, 1):
-    arr[i] = 1
-
-if number <= 3:
-    print(arr[number])
-else:
-    for num in range(4, number + 1):
-        if num % 6 == 0:  # 6의 배수일경우
-            arr[num] = arr[num // 3] + arr[num // 2]
-        elif num % 3 == 0:
-            arr[num] = arr[num // 3] + 1
-        elif num % 2 == 0:
-            arr[num] = arr[num // 2] + 1
-        else:
-            arr[num] = arr[num - 1] + 1
-
-    print(arr[number])
+print(arr[number])
