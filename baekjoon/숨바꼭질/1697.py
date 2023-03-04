005199aff4bb8b5dc1191674a45f44273c659ada
MoonGoon <moongoon.cnu@gmail.com>
2023-03-04 20:56:06 +0900

    ✅ Solve: 숨바꼭질 1

--- baekjoon/숨바꼭질/1697.py
+++ baekjoon/숨바꼭질/1697.py
@@ -1,21 +1,22 @@
 from collections import deque
-n, k = map(int, input().split())
-queue = deque()
-queue.append(n)
-time = 0
-# 수빈이가 동생보다 앞에있어서 뒤로만 가면 되는 경우
-if n > k :
-    print(n-k)
 
-while (n != k):
-    n = n * 2
-    time += 1
-    if (n - 1 > k):
-        n = queue.pop()
-        n -= 1
-        queue.append(n)
-    elif (n - 1 == k):
-        n -= 1
-        time += 1
-        
-print(time)
+def bfs(node):
+    queue = deque([node])
+
+    while queue:
+        now = queue.popleft()
+        # visited[now] 가 0 이면 아직 방문하지 않음
+        if now == brother:
+            print(visited[now])
+            break
+        for next_node in (now -1 , now + 1, now * 2):
+            # 다음 방문 위치가 범위 안에 있는지 확인하고, visited[next_node]가 0이면 아직 방문하지 않았으므로 추가해줌
+            if 0 <= next_node < 100001 and not visited[next_node]:
+                visited[next_node] = visited[now] + 1
+                queue.append(next_node)
+
+
+subin, brother = map(int, input().split(' '))
+visited = [0 for _ in range(100001)]
+result = 0
+bfs(subin)
