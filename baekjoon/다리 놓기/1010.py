d223c152a20787e826a1ad2088e20ecede076ecb
MoonGoon <moongoon.cnu@gmail.com>
2024-01-19 09:38:18 +0900

    ✅ Solve: 1010.py

--- baekjoon/다리 놓기/1010.py
+++ baekjoon/다리 놓기/1010.py
@@ -1,26 +1,14 @@
+import sys
 import math
+input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T):
-    N, M = map(int, input().split(' '))
-    # bridge = math.factorial(M) // (math.factorial(N) * math.factorial(M-N))
-    # print(bridge)
+t = int(input())
 
+def calcSite(n, m):
+    result = math.comb(m, n)
+    print(result)
+    return
 
-def bridge(n, m):
-    dp = [[0 for _ in range(m + 1)] for _ in range(n+1)]
-
-    for i in range(1, m+1):
-        dp[1][i] = i
-    
-    for j in range(2, n+1):
-        for k in range(j, m+1):
-            for l in range(k, j-1, -1):
-
-
-'''
-N 개 M 개 사이트 존재하고
-다리는 교차 불가능 
-N개 기준으로 해야함
-
-'''
+for _ in range(t):
+    n, m = map(int, input().split())
+    calcSite(n, m)
