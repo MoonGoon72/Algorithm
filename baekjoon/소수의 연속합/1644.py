52b0914c401ec1e721d1cdd98ebd25b0aa5eaca5
MoonGoon72 <moongoon.cnu@gmail.com>
2025-02-22 14:02:59 +0900

    ✅ solve: 소수의 연속합
    시간을 줄이는 방법을 생각하는게 어려웠음

---
+++ baekjoon/소수의 연속합/1644.py
@@ -0,0 +1,51 @@
+import sys, math
+input = sys.stdin.readline
+
+n = int(input())
+answer = 0
+
+if n == 1:
+    print(0)
+    exit(0)
+
+def prime_numbers(num):
+    tmp = [True for _ in range(num + 1)]
+    n_sqrt = int(math.sqrt(num)) +1 
+    for i in range(2, n_sqrt):
+        if not tmp[i]:
+            continue
+        
+        counter = 2
+        while i * counter <= num:
+            if tmp[i * counter]:
+                tmp[i * counter] = False
+            counter += 1
+    return tmp
+
+primes = prime_numbers(n)
+
+start = 2
+end = 2
+summation = start
+
+while start <= end:
+    if summation == n:
+        answer += 1
+        summation -= start
+        start += 1
+        while start <= n and primes[start] == False:
+            start += 1
+    elif summation < n:
+        end += 1
+        while end <= n and primes[end] == False:
+            end += 1
+        if end > n:
+            break
+        summation += end
+    elif summation > n:
+        summation -= start
+        start += 1
+        while start <= n and primes[start] == False:
+            start += 1
+
+print(answer)
