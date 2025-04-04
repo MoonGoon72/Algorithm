c2e6ea18c8968a7ed9252feb7abc9bb469e1501d
MoonGoon72 <moongoon.cnu@gmail.com>
2025-04-04 10:39:11 +0900

    ✅ 이코테: 팀 결성

---
+++ 이코테/다양한 그래프 알고리즘/실전 문제/팀_결성.py
@@ -0,0 +1,44 @@
+import sys
+input = sys.stdin.readline
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+n, m = map(int, input().split())
+parent = [i for i in range(n + 1)]
+
+for _ in range(m):
+    o, a, b = map(int, input().split())
+    if o:
+        a = find(a)
+        b = find(b)
+        print("YES") if a == b else print("NO")
+    else:
+        union(a, b)
+
+"""
+7 8
+0 1 3
+1 1 7
+0 7 6
+1 7 1
+0 3 7
+0 4 2
+0 1 1
+1 1 1
+
+NO
+NO
+YES
+"""
