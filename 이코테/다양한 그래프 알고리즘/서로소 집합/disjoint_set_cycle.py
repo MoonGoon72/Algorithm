3b0653e3ba60c858bb0ff090d6825d9d7cf5bcaf
MoonGoon72 <moongoon.cnu@gmail.com>
2025-04-04 10:11:17 +0900

    이코테: 서로소 집합 사이클 판별 알고리즘

---
+++ 이코테/다양한 그래프 알고리즘/서로소 집합/disjoint_set_cycle.py
@@ -0,0 +1,29 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().split())
+parent = [i for i in range(v + 1)]
+
+# 사이클 발생 여부 파악
+cycle = False
+
+for _ in range(e):
+    a, b = map(int, input().split())
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
+
+print("사이클이 발생하였습니다.") if cycle else print("사이클이 발생하지 않았습니다.")
