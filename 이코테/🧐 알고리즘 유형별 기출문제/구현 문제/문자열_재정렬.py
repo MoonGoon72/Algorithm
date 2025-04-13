a759244db052f0854cefd08f28c65ca56cd30e53
MoonGoon72 <moongoon.cnu@gmail.com>
2025-04-13 11:11:34 +0900

    📝 주석 추가

--- 이코테/🧐 알고리즘 유형별 기출문제/구현 문제/문자열_재정렬.py
+++ 이코테/🧐 알고리즘 유형별 기출문제/구현 문제/문자열_재정렬.py
@@ -4,7 +4,7 @@ def solution(data):
     num = 0
     arr = []
     for s in data:
-        if s < "A":
+        if s < "A": # 혹은 s.isAlpha() 로 알파벳 여부 판단 가능
             num += int(s) 
         else:
             arr.append(s)
