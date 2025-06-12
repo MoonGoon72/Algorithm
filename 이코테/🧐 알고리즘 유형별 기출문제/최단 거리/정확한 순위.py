f8e88a1b5844b27a461bc5190314b4f279cfca1a
MoonGoon72 <moongoon.cnu@gmail.com>
2025-06-12 11:13:44 +0900

    ✅ 이코테: 정확한 순위

---
+++ 이코테/🧐 알고리즘 유형별 기출문제/최단 거리/정확한 순위.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+inf = int(1e9)
+distance = [[inf] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    distance[i][i] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    distance[a][b] = 1
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j])
+
+answer = 0
+
+for i in range(1, n + 1):
+    count = 0
+    for j in range(1, n + 1):
+        if distance[i][j] != inf or distance[j][i] != inf:
+            count += 1
+    if count == n:
+        answer += 1
+
+print(answer)
